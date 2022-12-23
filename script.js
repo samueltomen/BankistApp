5da2ddad01513a8cb4575600d596f7c9ec03bb8e
Dytsaw <tomennana.samuel@outlook.fr>
2022-12-23 09:59:31 +0100

    looping arrays for each method

--- script.js
+++ script.js
@@ -73,43 +73,78 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 ////////////////////////////////////////////////
 
-// SLICE 
-// ==> Affiche ['b', 'c', 'd', 'e']
-let arr = ['a', 'b', 'c', 'd', 'e'];
-console.log(arr.slice(1));
+// FOR OF METHOD
+for (const movement of movements) {
+    if(movement > 0){
+        console.log(`Vous avez déposer ${movement}`);
+    }else{
+        // Math.abs prend la valeur absolue retire le signe (-)
+        console.log(`Vous avez retirer ${Math.abs(movement)}`);
+    }
+}
 
-// ==> Affiche ['e']
-console.log(arr.slice(-1));
 
-// ==> Affiche ['b','c'] 
-console.log(arr.slice(1,-2));
+// FOR EACH METHOD
+movements.forEach(function(movement){
+    if(movement > 0){
+        console.log(`Vous avez déposer ${movement}`);
+    }else{
+        // Math.abs prend la valeur absolue retire le signe (-)
+        console.log(`Vous avez retirer ${Math.abs(movement)}`);
+    }
+})
 
-// SPLICE
-// ==> Affiche : ['c', 'd', 'e']
-console.log(arr.splice(2));
 
-// ==> Affiche : ['a','b]
-console.log(arr);
 
-// ==> Affiche ['a']
-arr.splice(-1)
-console.log(arr);
 
-// REVERSE
 
-//  Affiche ==> ['n', 'm', 'l', 'k', 'j']
-let arr2 = ['j', 'k', 'l', 'm', 'n'];
-console.log(arr2.reverse());
 
-// CONCAT
-// Fonctionne de la même facon que le spread operator
-const letters = arr.concat(arr2)
-console.log(letters); 
-console.log([...arr,...arr2]); //SAME
 
-// JOIN
-// ==> Affiche a - n - m - l - k - j
-console.log(letters.join(' - '));
+
+
+
+
+
+
+
+
+// // SLICE 
+// // ==> Affiche ['b', 'c', 'd', 'e']
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// console.log(arr.slice(1));
+
+// // ==> Affiche ['e']
+// console.log(arr.slice(-1));
+
+// // ==> Affiche ['b','c'] 
+// console.log(arr.slice(1,-2));
+
+// // SPLICE
+// // ==> Affiche : ['c', 'd', 'e']
+// console.log(arr.splice(2));
+
+// // ==> Affiche : ['a','b]
+// console.log(arr);
+
+// // ==> Affiche ['a']
+// arr.splice(-1)
+// console.log(arr);
+
+// // REVERSE
+
+// //  Affiche ==> ['n', 'm', 'l', 'k', 'j']
+// let arr2 = ['j', 'k', 'l', 'm', 'n'];
+// console.log(arr2.reverse());
+
+// // CONCAT
+// // Fonctionne de la même facon que le spread operator
+// const letters = arr.concat(arr2)
+// console.log(letters); 
+// console.log([...arr,...arr2]); //SAME
+
+// // JOIN
+// // ==> Affiche a - n - m - l - k - j
+// console.log(letters.join(' - '));
 
 
 
