87385f4770bd98927db6d35299845a20a45ea36e
Dytsaw <tomennana.samuel@outlook.fr>
2023-01-02 14:40:04 +0100

    The filter method

--- script.js
+++ script.js
@@ -81,13 +81,17 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
-const user = 'Steven Thomas Williams'; //stw
-const username = user
-    .toLowerCase()
-    .split(' ')
-    .map(name => name[0])
-    .join('')
-console.log(username);
+const createUsernames = function (accs) {
+    accs.forEach(function (acc) {
+        acc.username = acc.owner
+            .toLowerCase()
+            .split(' ')
+            .map((name) => name[0])
+            .join('');
+        console.log(acc.username);
+    });
+};
+// createUsernames(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -123,4 +127,19 @@ const movementsDescriptions = movements.map((mov, i) => {
     }
 });
 
-console.log(movementsDescriptions);
+// console.log(movementsDescriptions);
+
+const deposits =  movements.filter(function(mov){
+    return mov > 0
+})
+
+const withdrawals = movements.filter(function(mov){
+    return mov <= 0;
+})
+
+// // Methode For Of
+// const depositsFor = [] 
+// for(const mov of movements) if (mov>0) depositsFor.push(mov)
+// console.log(depositsFor);
+console.log(withdrawals);
+
